64ab504a11eadf2fa4d0cd03d25722169626cfba
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2025-10-15 13:26:25 +0900

    update

---
+++ 01_jquery_1/js/app.js
@@ -0,0 +1,13 @@
+// TODO: JSの復習: id=message に「はじめます」を表示
+
+// TODO: jQueryで、DOM読み込み後にアラート表示
+// 1) jqdoc と入力
+// 2) 「jqDocReadyShort」を選択
+
+// TODO:DOM読み込み前の処理: アラートで「はじまるよ」を表示
+
+// バニラJSで、DOM読み込み後にアラート表示
+// 「DOMContentLoaded」の場合
+// document.addEventListener("DOMContentLoaded", function () {
+//     alert("バニラJS")
+// })
