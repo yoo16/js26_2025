31f460e5235c0ba9d59c38ef3ac07c9d14c59450
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2025-12-04 13:13:10 +0900

    update

--- 06_jquery_menu/js/dropdown.js
+++ 06_jquery_menu/js/dropdown.js
@@ -1,13 +1,14 @@
 $(function () {
-    // メニュー取得: class=menu
+    // メインメニュー取得: class=menu
     const navMenus = $(".menu");
 
     // TODO:ホバーでサブメニュー表示・非表示
     navMenus.hover(
         function () {
-            // TODO: 自分自身: $(this) の子要素 ul を表示
+            // TODO: 自分自身: $(this) の子要素（サブメニュー） ul を表示
             // stop() で誤動作防止
             // slideDown() アニメーション
+            $(this).children('ul').stop().slideDown()
         },
         function () {
             // TODO: 自分自身: $(this) の子要素 ul を非表示
