0f19fd4cc9c67f32d9cfa80a4f8d9cbdd08c01e8
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2026-01-29 13:16:14 +0900

    update

--- 08_jquery_parallax/js/parallax1.js
+++ 08_jquery_parallax/js/parallax1.js
@@ -3,21 +3,23 @@ $(document).ready(function () {
     const $parallaxContent = $('#parallaxContent');
 
     // TODO: スクロールイベントの監視
-    $(window).on('', function () {
+    $(window).on('scroll', function () {
         // TODO: スクロール位置を取得: $(window).scrollTop()
-        const scrollY = 0;
+        const scrollY = $(window).scrollTop();
         // TODO: 速度を調整: scrollY * 0.5
-        const speed = 0;
+        const speed = scrollY * 0.5;
 
         // TODO: パララックス対象の高さを取得: $parallax.outerHeight()
-        const parallaxHeight = 0;
+        const parallaxHeight = $parallax.outerHeight();
 
         // TODO: 背景画像を移動: $parallax.css()
         // background-position-y: -speed + 'px'
+        $parallax.css('background-position-y', -speed + 'px')
 
         // 文字コンテンツの移動（スクロールに合わせてゆっくり追従）
         const translateY = Math.min(parallaxHeight / 2, scrollY * 0.2) - 200;
         // TODO: $parallaxContent.css()
         // transform: translateY
+        $parallaxContent.css('transform', 'translateY(' + translateY + 'px)');
     });
 });
