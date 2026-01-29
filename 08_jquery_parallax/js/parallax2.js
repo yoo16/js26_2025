5a3848b6f6ddbce13067daf9018f1996355147b8
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2026-01-29 13:40:21 +0900

    update

--- 08_jquery_parallax/js/parallax2.js
+++ 08_jquery_parallax/js/parallax2.js
@@ -54,5 +54,6 @@ $(document).ready(function () {
     // data-animate属性を持つ要素を監視
     $('[data-animate]').each(function () {
         // TODO: 監視開始: observe(this)
+        observer.observe(this)
     });
 });
